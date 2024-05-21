105f31a7b279dbbed81bf00451467b2758f143b9
Svetlana <radalana415@gmail.com>
2024-05-21 18:39:40 +0200

    Finish logik for Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  public email: string = '';
-  public password: string = '';
-  public password_confirm: string = '';
+  fb = inject(FormBuilder); //injection statt in constructor zu initialisieren
+  formLogIn = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    }, 
+    {
+      validators: this.checkLogin //custom validator
+    }
+  );
+
+  checkLogin(formGroup: AbstractControl): ValidationErrors | null {
+    const login = formGroup.get('email')?.value;
+    const password = formGroup.get('password')?.value;
+
+    const result = (login === 'test@test.at') && (password === '12345678');
+    
+    if (result === true) {
+      console.log('Login successful');
+    } else {
+      console.log('Login failed');
+    }
+    return result === true? null : {'invalid': true };
+  }
 }
