3b8f740f1b6f400dad147e6271df04e9156a05fd
Svetlana <radalana415@gmail.com>
2024-05-29 19:41:36 +0200

    Landing page add buttons in template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { HttpClientModule } from '@angular/common/http';//client auf express-ser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SignupComponent } from './signup/signup.component';
-import {MatFormFieldControl, MatFormFieldModule} from '@angular/material/form-field'; //for form
+import {MatFormFieldModule} from '@angular/material/form-field'; //for form
 import {MatInputModule} from '@angular/material/input';//for form
 import {MatIconModule} from '@angular/material/icon';//for form icons
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import { LandingPageComponent } from './landing-page/landing-page.component'; 
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
@@ -33,7 +33,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     MatButtonModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatCardModule
+    MatCardModule,
   ],
   providers: [
     provideAnimationsAsync()
